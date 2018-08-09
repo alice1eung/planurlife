78648c8cf073456836eab7bafb99e42da883dcba
Alice Leung <alice1eung@v1020-wn-79-12.campus-dynamic.uwaterloo.ca>
2018-08-08 22:45:05 -0400

    updated README.md and more random testing

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ A web app to plan all your future school terms.
 https://github.com/uWaterloo/api-documentation/blob/master/v2/courses/subject_catalog_number_prerequisites.md
 https://github.com/uWaterloo/api-documentation/blob/master/v2/courses/subject_catalog_number.md
 https://github.com/uWaterloo/api-documentation/blob/master/v2/terms/term_courses.md
+http://dev.yathit.com/ydn-db/doc/setup/
--- js/test.js
+++ js/test.js
@@ -1,3 +1,18 @@
-$.get('https://ugradcalendar.uwaterloo.ca/page/MATH-Bachelor-of-Computer-Science-1', null, function(text){
-  
-});
+
+var temp;
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://ugradcalendar.uwaterloo.ca/page/MATH-Bachelor-of-Computer-Science-1"; // site that doesn’t send Access-Control-*
+fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+.then(response => response.text())
+.then(contents => temp = contents)
+.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+
+var textToSave = temp;
+
+var hiddenElement = document.createElement('a');
+
+hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+hiddenElement.target = '_blank';
+hiddenElement.download = 'myFile.txt';
+hiddenElement.click();
