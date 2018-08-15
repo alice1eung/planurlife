640ab611740143a52e813388147860ac804e4b43
Alice Leung <alice1eung@Alices-MacBook-Pro.local>
2018-08-14 21:45:04 -0400

    testing

--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const url = "https://ugradcalendar.uwaterloo.ca/page/MATH-Bachelor-of-Computer-Science-1"; // site that doesn’t send Access-Control-*
 fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
 .then(response => response.text())
-.then(contents => temp = contents)
+.then(contents => console.log(contents))
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
 
